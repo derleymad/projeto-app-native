7864f83143818976e1edf716ae641c296027b919
Wanderson Valentim <wanderson200108@gmail.com>
2023-04-21 17:37:24 -0300

    add src folder

--- curativar/App.tsx
+++ curativar/App.tsx
@@ -3,6 +3,7 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 function Feed() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
