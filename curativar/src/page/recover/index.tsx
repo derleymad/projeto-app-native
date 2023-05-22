35f278c7360dbc14fbff9dda65c00fe2d203f9ff
Wanderson Valentim <wanderson200108@gmail.com>
2023-05-22 11:57:54 -0300

    [feature/add-navigation]: Adicionar contexto de navegação entre as telas

---
+++ curativar/src/page/recover/index.tsx
@@ -0,0 +1,8 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../types/navigation";
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Recover'>;
+
+export default function Recover({navigation}: Props){
+  return <></>
+}
---
+++ curativar/src/page/register/index.tsx
@@ -0,0 +1,8 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../types/navigation";
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Register'>;
+
+export default function Register({navigation}: Props){
+  return <></>
+}
