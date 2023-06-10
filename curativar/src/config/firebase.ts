d4dcb0c33d26cd2d82efe3582f7326cdb29850ae
Wanderson Valentim <wanderson200108@gmail.com>
2023-06-10 10:02:05 -0300

    Adicionando configuração firebase

---
+++ curativar/src/config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCh0BkjrxIA2OE2nD0MpMVfS-y29B37qZA",
+  authDomain: "projeto-app-native.firebaseapp.com",
+  databaseURL: "https://projeto-app-native-default-rtdb.firebaseio.com/",
+  projectId: "projeto-app-native",
+  storageBucket: "projeto-app-native.appspot.com",
+  messagingSenderId: "417378219342",
+  appId: "1:417378219342:web:54305deb1a24771d50da1f"
+};
+
+const app = initializeApp(firebaseConfig); 
